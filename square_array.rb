e782c0a67bdf20296666fdd42ed40341ac580f5e
Thomas Wright <twwright@gmail.com>
2019-10-19 18:17:16 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   squared_array = []
-  square_array.each do |number_in|
-    squared = number_in**2
-    squared_array.push(squared)
+  square_array.each do |input|
+      squared = input**2
+      squared_array.push(squared)
   end
 end
